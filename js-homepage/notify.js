7314b1b08ea55b9898750b475837493755102a3c
Iam-tomiwa <ajayiemmanuel630@gmail.com>
2021-02-04 00:37:49 +0100

    fix notify btn

--- js-homepage/notify.js
+++ js-homepage/notify.js
@@ -39,7 +39,7 @@ subscribe.addEventListener("submit", e => {
     })
     .finally(_ => {
       subscribe.reset();
-      subscribeBtn.textContent = "Login";
+      subscribeBtn.textContent = "Notify Me!";
       subscribeBtn.disabled = false;
     });
 });
